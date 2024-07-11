70a28b604699aff5b2587e23c86fc57a11e967c1
moheranus <danielshobe90@gmail.com>
2024-07-11 14:42:50 +0300

    added telegraf

--- Bot/bot.js
+++ Bot/bot.js
@@ -1,7 +1,7 @@
 const { Telegraf } = require("telegraf");
 const TOKEN = "7237558909:AAHWIk34KWaVIexGLN3nF9krEUb9m8yx23M";
 const bot = new Telegraf(TOKEN);
-
+// we address the web app link
 const web_link = "https://celebrated-torte-184681.netlify.app/";
 
 bot.start((ctx) =>
